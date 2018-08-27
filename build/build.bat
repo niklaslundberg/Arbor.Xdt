a8972079e53c149cd508f4b02db5939fc5837558
Niklas Lundberg <info@nlundberg.se>
2018-08-27 22:00:51 +0200

    Updated build script.

--- build/build.bat
+++ build/build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 SET Arbor.X.MSBuild.NuGetRestore.Enabled=true
 SET Arbor.X.NuGet.Package.Artifacts.CreateOnAnyBranchEnabled=true
-CALL "%~dp0\Build.exe"
+CALL arbor-build
 
 EXIT /B %ERRORLEVEL%
